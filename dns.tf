f32b16b5c82b29f5d073f9e45a8f12a91ddd14bb
bhaworth <ben@benhaworth.com>
2021-04-01 17:10:01 +0100

    dns for LB

--- dns.tf
+++ dns.tf
@@ -1,19 +1,19 @@
-# resource "oci_dns_rrset" "lb_a_record" {
-#   count = var.create_dns ? 1 : 0
+resource "oci_dns_rrset" "lb_a_record" {
+  count = var.create_dns ? 1 : 0
 
-#   domain          = "${local.Fn4_env_name}.oci.fn4dev.ml"
-#   rtype           = "A"
-#   zone_name_or_id = local.Fn4dev_ml_dns_zone_id
-# 
-#   items {
-#     domain = "${local.Fn4_env_name}.oci.fn4dev.ml"
-#     rtype  = "A"
-#     rdata  = local.Fn4_lb_public_ip
-#     ttl    = 30
+  domain          = "${local.Fn4_env_name}.oci.fn4dev.ml"
+  rtype           = "A"
+  zone_name_or_id = local.Fn4dev_ml_dns_zone_id
 
-#   }
+  items {
+    domain = "${local.Fn4_env_name}.oci.fn4dev.ml"
+    rtype  = "A"
+    rdata  = local.Fn4_lb_public_ip
+    ttl    = 30
 
-# }
+  }
+
+}
 
 resource "oci_dns_rrset" "bastion_a_record" {
   count = var.create_dns ? 1 : 0
